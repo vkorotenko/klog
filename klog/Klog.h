635835ae5ad0e06bd82f704bdbe36e397191a117
vkorotenko <koroten@ya.ru>
2013-03-30 14:08:10 +0400

    Remove windows specific header. Read log level and log file name from file klog.cfg

--- klog/Klog.h
+++ klog/Klog.h
@@ -10,16 +10,18 @@
 
 
 #pragma once
-#include <Windows.h>
 #include <stdio.h>
 #include <stdarg.h>
-#include <atlstr.h>
+#include <time.h>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+using namespace std;
 
 template <class T>
 class Singleton
 {
 	static T* _self;
-	static int _refcount;
 protected:
 	Singleton(){}
 	virtual ~Singleton(){_self=NULL;}
@@ -32,49 +34,86 @@ template <class T>
 T*  Singleton<T>::_self = NULL;
 
 template <class T>
-int  Singleton<T>::_refcount=0;
-
-template <class T>
 T*  Singleton<T>::Instance()
 {
 	if(!_self)
 		_self=new T;
-	_refcount++;
 	return _self;
 }
 
 template <class T>
 void  Singleton<T>::FreeInst()
 {
-	if(--_refcount==0)
-		delete this;
+	delete this;
 }
 
 
 class CKlog: public Singleton<CKlog>
 {
 private:
-	HANDLE _file;
+	ofstream _outstream;
+	string _logName;
 	int _level;
-	// Read log level from registry and initialize log level 
+	// Read log level from file and initialize log level and log file name 
 	void ReadLevel(){
-
-		DWORD lRv;
-		const CHAR* runKey="Software\\KVN"; 
-		DWORD val=0;
-		DWORD sz= sizeof(DWORD);
-		LPDWORD pdwType =0;
-		lRv = RegGetValue(HKEY_CURRENT_USER,runKey,"level",RRF_RT_REG_DWORD,pdwType, &val,&sz);
-		_level = (int) val;
+		ifstream ins;
+		ins = ifstream("klog.cfg");
+		char buff[255];
+		while(ins.getline(buff,255)){
+			ParseConfig(buff);
+		}
+		ins.close();
 	}
-
+	void Tokenizer(string source, string &key, string &value){
+		int pos = source.find("=");
+		key = source.substr(0,pos);
+		value = source.substr(pos+1);
+	}
+	void ParseConfig(const char* str){
+		string buff = str;
+		int commentPos = buff.find("#");
+		if(commentPos > -1){
+			buff = buff.substr(0,commentPos);
+		}
+		if(buff.find("level")< -1){
+			string key,value;
+			Tokenizer(buff,key,value);
+			_level = 0;
+			istringstream ( value ) >> _level;
+		}
+		if(buff.find("level")< -1){
+			string key,value;
+			Tokenizer(buff,key,value);
+			_level = 0;
+			istringstream ( value ) >> _level;
+		}
+		if(buff.find("filename")< -1){
+			string key,value;
+			Tokenizer(buff,key,value);
+			_logName = value;
+		}
+		
+	}
+	
+	void spf(std::string &s, const std::string fmt, va_list arg)
+	{
+		int n, size=100;
+		bool b=false;
+		while (!b)
+		{
+			s.resize(size);
+			n = vsnprintf((char*)s.c_str(), size, fmt.c_str(), arg);
+			if ((n>0) && ((b=(n<size))==true)) s.resize(n); else size*=2;
+		}
+	}
 
 protected:
 
 	// create log file and setup internal variables
 	CKlog(){
-		_file = CreateFile ("trace.log", FILE_APPEND_DATA, FILE_SHARE_READ, NULL, OPEN_ALWAYS,FILE_ATTRIBUTE_NORMAL, 0);
+		
 		ReadLevel();
+		_outstream =  std::ofstream(_logName,std::ios_base::app | std::ios_base::out);
 	}
 	friend class Singleton<CKlog>;
 public:
@@ -85,27 +124,22 @@ public:
 	void Log(int level, const char* message, ...){
 
 		if(level >= _level){
-			va_list arglist;
-			va_start(arglist, message);
-			SYSTEMTIME time;
-			GetSystemTime(&time);
+			
+			time_t current_time;
+			current_time = time(NULL);
+			struct tm * timeinfo;
+			timeinfo = localtime ( &current_time );
 
-			CAtlString strBuff;
-			strBuff.Format("%02d/%02d/%4d %02d:%02d:%02d.%04d\t%d\t",time.wDay,
-				time.wMonth,
-				time.wYear, 
-				time.wHour,
-				time.wMinute,
-				time.wSecond,
-				time.wMilliseconds,
-				level);
-			CAtlString strMsg;
-			strMsg.FormatV(message,arglist);
-			strBuff.Append(strMsg);
-			strBuff.Append("\r\n");
-			DWORD numb_bytes;
-			WriteFile(_file, strBuff, strBuff.GetLength(), &numb_bytes, NULL);
+			_outstream << timeinfo->tm_mday << "/" << timeinfo->tm_mon <<"/" << timeinfo->tm_year + 1900 << " " <<
+				timeinfo->tm_hour << ":" << timeinfo->tm_min << ":" <<timeinfo->tm_sec << "\t" << level << "\t";
+
+			string buffer;
+			va_list arglist=0;
+			va_start(arglist, message);
+			spf(buffer,message,arglist);
 			va_end(arglist);
+			_outstream << buffer << endl;
+			_outstream.flush();
 		}
 	};
 };
--- klog/main.cpp
+++ klog/main.cpp
@@ -1,7 +1,5 @@
 #include "Klog.h"
-int main(int argc, char** argv){
-	
-	LOG(255,"test %s", "hello" );
+void main(){
+	LOG(255,"test %s %d", "zzzz",22 );
 	LOG(255,"test");
-
 }
